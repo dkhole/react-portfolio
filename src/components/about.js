0e409f3e84dfc45ce8259bb2eb8f4be38091c88e
dkhole <dkhlee419@gmail.com>
2021-01-05 15:08:50 +1100

    finished about page

--- src/components/about.js
+++ src/components/about.js
@@ -4,6 +4,7 @@ import Hamburger from './hamburger.js';
 import RightCorner from './rightCorner.js';
 import github from '../img/github.png';
 import styles from '../styles/about.module.css';
+import { ReactComponent as Face} from '../img/Ellipse2.svg';
 
 export default function About() {
     return (
@@ -15,15 +16,25 @@ export default function About() {
         </div>
 
         <div id={styles.profile}>
-          <img alt="profile"></img>
-          <div id={styles.info}>
-            <div id={styles.name}>Daniel Jungho Lee</div>
-            <div id={styles.country}>SYD,NSW</div>
-            <div id={styles.nationality}>Australian</div>
-            <div id={styles.email}>dkhlee419@gmail.com</div>
-            <div id={styles.github}>www.github.com/dkhole</div>
-            <div id={styles['skill-title']}>Skills</div>
-            <div id={styles.skills}>HTML, CSS, SASS, JS, TS, REACT, GATSBY, Node.js, Express.js, Heroku, Netlify, SQL</div>
+          <div id={styles.first}>
+            <div id={styles['face-wrapper']}>
+              <Face id={styles.face}/>
+            </div>
+          </div>
+          <div id={styles.second}>
+            <div id={styles.intro}>
+              <div id={styles.name}>Daniel Jungho Lee</div>
+              <div id={styles.country}>SYD,NSW</div>
+            </div>
+            <div id={styles.details}>
+              <div id={styles.nationality}><span>Nationality:</span>Australian</div>
+              <div id={styles.email}><span>Email:</span>dkhlee419@gmail.com</div>
+              <div id={styles.github}><span>Github:</span>www.github.com/dkhole</div>
+            </div>
+            <div id={styles['skills-wrap']}>
+              <div id={styles['skill-title']}>Skills:</div>
+              <div id={styles.skills}>HTML, CSS, SASS, JS, TS, REACT, GATSBY, Node.js, Express.js, Heroku, Netlify, SQL</div>
+            </div>
           </div>
         </div>
 
