b5bd734f22b8973d4a8f3bc1f1f9fcdd42974072
dkhole <dkhlee419@gmail.com>
2021-03-11 14:41:23 +1100

    added landing page transitions

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import styles from '../styles/home.module.css';
 import LeftCorner from './leftCorner.js';
 import Hamburger from './hamburger.js';
@@ -7,6 +7,7 @@ import { ReactComponent as Face} from '../img/face.svg';
 import github from '../img/github.png';
 import buttonStyles from '../styles/button.module.css';
 import { Link } from "react-router-dom";
+import ReactTypingEffect from 'react-typing-effect';
 
 const Button = () => {
   return (
@@ -15,6 +16,26 @@ const Button = () => {
       <Link to="/projects"><input type="button" value="Projects"></input></Link>
     </div>
   );
+}
+
+const drawFaceSvg = () => {
+  const lines = document.querySelectorAll('.face-line');
+  lines.forEach((line) => {
+    const length = line.getTotalLength();
+    // Clear any previous transition
+    line.style.transition = line.style.WebkitTransition = 'none';
+    // Set up the starting positions
+    line.style.strokeDasharray = length + ' ' + length;
+    line.style.strokeDashoffset = length;
+    // Trigger a layout so styles are calculated & the browser
+    // picks up the starting position before animating
+    line.getBoundingClientRect();
+    // Define our transition
+    line.style.transition = line.style.WebkitTransition =
+      'stroke-dashoffset 2s ease-in-out';
+    // Go!
+    line.style.strokeDashoffset = '0';
+  });
 }
 
 export default function Home({ isNight, toggleNightMode, navStatus, setNavStatus }) {
@@ -29,6 +50,11 @@ export default function Home({ isNight, toggleNightMode, navStatus, setNavStatus
     }
   }, [isNight]);
 
+  useEffect(() => {
+    //draw face svg effect
+    drawFaceSvg();
+  }, []);
+
   if(navStatus) {
     return (
       <div id={styles.home} style={{left: "-200px"}}>
@@ -36,7 +62,6 @@ export default function Home({ isNight, toggleNightMode, navStatus, setNavStatus
           <Hamburger toggleNightMode={toggleNightMode} isNight={isNight} navStatus={navStatus} setNavStatus={setNavStatus}/>
           <div id={styles['title-wrap']}>
               <span id={styles.name}>Daniel J<span>.</span> Lee</span>
-              <span id={styles['sub-title']}>Web Development</span>
               <Button />
           </div>
           <div id={styles['face-wrap']}>
@@ -52,8 +77,30 @@ export default function Home({ isNight, toggleNightMode, navStatus, setNavStatus
           <LeftCorner />
           <Hamburger toggleNightMode={toggleNightMode} isNight={isNight} navStatus={navStatus} setNavStatus={setNavStatus}/>
           <div id={styles['title-wrap']}>
-              <span id={styles.name}>Daniel J<span>.</span> Lee</span>
-              <span id={styles['sub-title']}>Web Development</span>
+              {/*<span id={styles.name}>Daniel J<span>.</span> Lee</span>*/}
+              <ReactTypingEffect 
+                id={styles.name}
+                text={"Daniel J. Lee"} 
+                typingDelay={400}
+                eraseSpeed={100}
+                displayTextRenderer={(text) => {
+                    return (
+                      <span id={styles.name}>
+                        {
+                          text.split('').map((char, index) => {
+                            if(index === 8) {
+                              return <span>{char}</span>;
+                            } else {
+                              return <>{char}</>
+                            }
+                          })
+                        }
+                      </span>
+                    );
+                  }
+                }
+              />
+
               <Button />
           </div>
           <div id={styles['face-wrap']}>
